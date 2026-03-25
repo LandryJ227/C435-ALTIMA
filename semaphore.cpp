6e7df7e9d73182df520ecce22bb07ea7a197a18d
LandryJ227 <jacoblandry46@gmail.com>
2026-03-25 13:01:06 -0400

    implemented a basic circular queue

--- semaphore.cpp
+++ semaphore.cpp
@@ -1,5 +1,28 @@
 #include "semaphore.h"
+#include "queue.h"
+#include "task.h"
+#include <iostream>
+using namespace std;
 
-down() {
-    if ()
+void semaphore::down(Task T) {
+    if (sema_value == 1) {
+        sema_value = 0;
+        //proceed
+    }
+    else {
+        sema_queue->enqueue(T);
+        //block this process until 1
+    }
+}
+
+void semaphore::up() {
+    sema_value = 1;
+    //wake up next process
+}
+
+void semaphore::dump (int level) {
+    cout << "\n##### Semaphore Dump #####" << endl;
+    cout << "Resource name: " << resource_name << endl;
+    cout << "Sema_value: " << sema_value << endl;
+    cout << "Sema_queue: " << sema_queue << endl;
 }
---
+++ task.h
@@ -0,0 +1,4 @@
+class Task {
+    char taskName[64];
+    char taskState[64];
+};

0ea82adb45f36c231ddb7dd993feddf23b63b648
rayearl-netizen <ryanearlbusiness@gmail.com>
2026-04-12 11:11:32 -0500

    mcb

--- Phase2 (Memory Management)/Source Files/MCB.h
+++ Phase2 (Memory Management)/Source Files/MCB.h
@@ -1,12 +1,11 @@
 #ifndef ALTIMA_MCB_H
 #define ALTIMA_MCB_H
-
-class scheduler;
-class IPC;
+#include "Sched.h"
+#include "IPC.h"
 
 class MCB {
 public:
-    scheduler* sched;
+    scheduler sched;
     IPC* messenger;
     //Add any other important semaphores or structures you want to access between structures
 
--- Phase2 (Memory Management)/Source Files/Queue.h
+++ Phase2 (Memory Management)/Source Files/Queue.h
@@ -1,14 +1,14 @@
 #pragma once
 
+
 class queue {
-public:
     static const int QUEUE_SIZE = 64; //Fixing max number of threads in the semaphore queue
     int taskQueue[QUEUE_SIZE];  //Array to support a circular array
     //Index variables to support circular array data structure
     int numOfTasks=0;
     int head=0;
     int tail=0;
-
+public:
     //Function to add an element to the back of the queue
     void enqueue(int T);
     //Function to remove and return the integer valoue at the front of the queue.

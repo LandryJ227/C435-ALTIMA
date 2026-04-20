cc33878e4b34eba4cb2c7c94d43c5cb2c1a1630a
LandryJ227 <jacoblandry46@gmail.com>
2026-04-20 12:22:25 -0400

    mmu

---
+++ Phase3 (Memory Management)/Source Files/mmu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jacob on 4/20/2026.
+//
+
+#include "mmu.h"
---
+++ Phase3 (Memory Management)/Source Files/mmu.h
@@ -0,0 +1,30 @@
+#ifndef ALTIMA_MMU_H
+#define ALTIMA_MMU_H
+
+
+class mmu {
+    class block {
+        char blockOfMem[64];
+        int taskAllocatedTo = -1;
+    };
+
+    block mainMem[16];
+
+
+    mmu(int size, char default_initial_value, int page_size);
+    ~mmu();
+    int Mem_Alloc(int size);
+    int Mem_Free(int memory_handle);
+    int Mem_Read(int memory_handle, char* ch);
+    int Mem_Write(int memory_handle, char ch);
+    int Mem_Read(int memory_handle, int offset_from_beg, int text_size, char *text);
+    int Mem_Write(int memory_handle, int offset_from_beg, int text_size, char *text);
+    int Mem_Left();
+    int Mem_Largest();
+    int Mem_Smallest();
+    int Mem_Coalesce();
+    int Mem_Dump(int starting_from, int num_bytes);
+};
+
+
+#endif //ALTIMA_MMU_H

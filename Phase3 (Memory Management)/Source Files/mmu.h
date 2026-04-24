7f5c943ea87acde8ba0f422f4596dbcda044eb48
rayearl-netizen <ryanearlbusiness@gmail.com>
2026-04-24 14:44:32 -0400

    simple mem dump working

--- Phase3 (Memory Management)/Source Files/mmu.h
+++ Phase3 (Memory Management)/Source Files/mmu.h
@@ -43,7 +43,7 @@ public:
     //~mmu();//NOT DONE
     int Mem_Alloc(int size, int task_id);//DONE
     int Mem_Free(int memory_handle);//DONE
-    int Mem_Read(int memory_handle, char* ch);//NOT DONE
+    int Mem_Read(int memory_handle, char* ch);//DONE
     int Mem_Write(int memory_handle, char ch);//NOT DONE
     int Mem_Read(int memory_handle, int offset_from_beg, int text_size, char *text);//NOT DONE
     int Mem_Write(int memory_handle, int offset_from_beg, int text_size, char *text);//NOT DONE
@@ -51,7 +51,7 @@ public:
     int Mem_Largest();//Julio
     int Mem_Smallest();//Julio
     int Mem_Coalesce();//NOT DONE
-    int Mem_Dump(int starting_from, int num_bytes);//Ryan
+    int Mem_Dump(int starting_from, int num_bytes);//DONE
 };
 
 

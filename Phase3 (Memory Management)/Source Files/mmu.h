61d5d76849c1db8ae33ef7d0208c8950f5a73c15
julegonz <jeghr0708@icloud.com>
2026-04-25 15:21:33 -0400

    adding author names to functions

--- Phase3 (Memory Management)/Source Files/mmu.h
+++ Phase3 (Memory Management)/Source Files/mmu.h
@@ -45,7 +45,7 @@ public:
     int Mem_Free(int memory_handle);//DONE
     int Mem_Read(int memory_handle, char* ch);//NOT DONE
     int Mem_Write(int memory_handle, char ch);//NOT DONE
-    int Mem_Read(int memory_handle, int offset_from_beg, int text_size, char *text);//NOT DONE
+    int Mem_Read(int memory_handle, int offset_from_beg, int text_size, char *text); // Julio
     int Mem_Write(int memory_handle, int offset_from_beg, int text_size, char *text);//NOT DONE
     int Mem_Left();//NOT DONE
     int Mem_Largest();//Julio

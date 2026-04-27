9fbb9481de883685da99724cb89bbc3b650ca794
LandryJ227 <jacoblandry46@gmail.com>
2026-04-26 23:39:46 -0400

    commit done

--- Phase3 (Memory Management)/Source Files/MCB.cpp
+++ Phase3 (Memory Management)/Source Files/MCB.cpp
@@ -1,8 +1,10 @@
 #include "MCB.h"
 #include "scheduler.h"
 #include "IPC.h"
+#include "mmu.h"
 
-MCB::MCB(scheduler* mainSched, IPC* mainIPC) {
+MCB::MCB(scheduler* mainSched, IPC* mainIPC, mmu* mainMem) {
     sched = mainSched;
     ipc = mainIPC;
+    mem = mainMem;
 }
